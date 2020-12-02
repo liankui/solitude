55d931f0ec68f0247cee1a5562eb34ec9d1cc174
liankui <yuekewin@github.com>
2020-12-02 20:53:25 +0800

    build: add gorm logMode

--- config/mysql.go
+++ config/mysql.go
@@ -5,7 +5,9 @@ import (
 	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 	"log"
+	"time"
 )
 
 var DB *DataBase
@@ -23,10 +25,18 @@ func openDB(user, pass, addr, dbname string) *gorm.DB {
 		DontSupportRenameIndex:    true,  // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
 		DontSupportRenameColumn:   true,  // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
 		SkipInitializeWithVersion: false, // 根据当前 MySQL 版本自动配置
-	}), &gorm.Config{})
+
+	}), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Warn), // 日志级别：Silent、Error、Warn、Info
+	})
 	if err != nil {
 		log.Fatalf("Database:%s connection error: %v", dbname, err)
 	}
+
+	sqlDB, _ := db.DB()                 // 连接池
+	sqlDB.SetMaxIdleConns(10)           // SetMaxIdleConns 设置空闲连接池中连接的最大数量
+	sqlDB.SetMaxOpenConns(100)          // SetMaxOpenConns 设置打开数据库连接的最大数量。
+	sqlDB.SetConnMaxLifetime(time.Hour) // SetConnMaxLifetime 设置了连接可复用的最大时间。
 
 	return db
 }
