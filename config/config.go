6eaf1bf4ab633f9e3710d92a7a6417668cc7ff54
liankui <yuekewin@gmail.com>
2020-12-27 21:52:07 +0800

    style

--- config/config.go
+++ config/config.go
@@ -20,13 +20,13 @@ func InitCfg(cfg string) {
 }
 
 func (c *Config) initViper() error {
-	viper.SetConfigName("config")         // name of config file (without extension)
-	viper.SetConfigType("yaml")           // REQUIRED if the config file does not have the extension in the name
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.SetConfigType("yaml")   // REQUIRED if the config file does not have the extension in the name
 	//viper.AddConfigPath("/etc/appname/")  // path to look for the config file in
 	//viper.AddConfigPath("$HOME/.appname") // call multiple times to add many search paths
-	viper.AddConfigPath(".")              // optionally look for config in the working directory
-	err := viper.ReadInConfig()           // Find and read the config file
-	if err != nil {                       // Handle errors reading the config file
+	viper.AddConfigPath(".")    // optionally look for config in the working directory
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
 		return fmt.Errorf("Fatal error config file: %s \n", err)
 	}
 	return nil
